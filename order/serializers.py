52700d685711aead83a2931d972bf392b64dea24
Pierre COURBIN <pierre.courbin@gmail.com>
2025-08-06 10:01:18 +0200

    Enhance serializers to include item details in representation for Order and Outfit models

--- order/serializers.py
+++ order/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from order.models import Order
-from item.serializers import ItemSerializer
+from item.serializers import ItemSerializer, Item
 
 class OrderSerializer(serializers.ModelSerializer):
-    #items = ItemSerializer(read_only=False,  many=True)
 
     class Meta:
         model = Order
         fields = ['id', 'status', 'items', 'created']
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['items'] = [ItemSerializer(Item.objects.get(id= int(i))).data for i in rep['items']]
+        return rep
--- outfit/serializers.py
+++ outfit/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from outfit.models import Outfit
-from item.serializers import ItemSerializer
+from item.serializers import ItemSerializer, Item
 
 class OutfitSerializer(serializers.ModelSerializer):
-    #items = ItemSerializer(read_only=False,  many=True)
-
     class Meta:
         model = Outfit
         fields = ['id', 'title', 'description', 'items', 'created']
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['items'] = [ItemSerializer(Item.objects.get(id= int(i))).data for i in rep['items']]
+        return rep
